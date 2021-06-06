5fc3104b00a14f5c88998d46c0b2cc1fe3ef2357
NakanoYuka <>
2021-06-06 17:41:03 +0000

    I did part1 of Problem3-4 and read the file

--- Exercise_1_problem_3_4.py
+++ Exercise_1_problem_3_4.py
@@ -9,7 +9,7 @@
 # 
 
 # YOUR CODE HERE 1 to read the file
-#raise NotImplementedError() 
+#raise NotImplementedError()
 import pandas as pd 
 data=pd.read_table("data/travelTimes_2015_Helsinki.txt",sep=';')
 #Check how many rows and columns there are:
